047e958dd12710dc5ee1d571827d914fd1020fab
Naomi Dax <imkadax@gmail.com>
2024-07-04 11:29:02 +0000

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,9 @@ function runGame(gameType) {
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
+    }else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
     };
@@ -68,7 +70,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if(operator === "x"){
+        return [operand1 * operand2, "multiply"];
+    }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -95,14 +99,20 @@ function incrementWrongAnswer() {
 
 }
 function displayAdditionQuestion(operand1, operand2) {
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
+
 }
 
 function displaySubtractQuestion() {
 
 }
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+
 }
